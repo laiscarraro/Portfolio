79d62d702b7d0bdcfda1c07c5c3e9c621ca62d48
laiscarraro <37347778+laiscarraro@users.noreply.github.com>
2019-07-14 17:38:48 -0300

    Add files via upload

--- music.js
+++ music.js
@@ -2,8 +2,7 @@ function setup() {
   var wave = new p5.Oscillator();
   wave.setType('triangle');
   wave.start();
-  wave.amp(0.5, 0.5);
+  wave.amp(0.5);
   wave.freq(240);
-  wave.start(1);
   wave.freq(460);
 }
