a1c25602595af9702d66953001a1e79470a3d8ff
laiscarraro <37347778+laiscarraro@users.noreply.github.com>
2019-07-14 17:43:56 -0300

    Add files via upload

--- music.js
+++ music.js
@@ -1,8 +1,23 @@
+var osc;
+
 function setup() {
-  var wave = new p5.Oscillator();
-  wave.setType('triangle');
-  wave.start();
-  wave.amp(0.5);
-  wave.freq(240);
-  wave.freq(460);
+  var osc = new p5.Oscillator();
+  osc.setType('triangle');
+  osc.amp(0);
+  playNote(240, 2);
+  playNote(440, 1);
+}
+
+function playNote(note, duration) {
+  osc.freq(note);
+  
+  // Fade it in
+  osc.fade(0.5,0.2);
+
+  // If we sest a duration, fade it out
+  if (duration) {
+    setTimeout(function() {
+      osc.fade(0,0.2);
+    }, duration-50);
+  }
 }
