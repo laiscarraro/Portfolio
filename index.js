6dc5ce665d399cdf350d0f6cad7ddad98ffb7779
Lais Carraro <laiscarraro@usp.br>
2021-04-03 20:35:31 -0300

    Add sound visualizer menu button

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var palette = [
 var menu;
 
 function setup() {
-  menu = createMenu(["Music Genetic Algorithm", "MIDI Player"], ["music.html", "midiPlayer.html"]);
+  menu = createMenu(["Music Genetic Algorithm", "MIDI Player", "Sound Visualizer"], ["music.html", "midiPlayer.html", "soundVisualizer.html"]);
 }
 
 function draw() {
