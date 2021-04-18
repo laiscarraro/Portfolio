223098d17f141db9bd1b8c5d56d3b3c11ebd3e66
Lais Carraro <laiscarraro@usp.br>
2021-04-18 20:20:42 -0300

    Add photos

--- chococast.js
+++ chococast.js
@@ -15,6 +15,10 @@ var amp_anna;
 var amp_hoffman;
 var amp_musica;
 
+var img_lais;
+var img_anna;
+var img_hoffman;
+
 function toggleSong() {
   if (lais.isPlaying()) {
     lais.pause();
@@ -34,6 +38,9 @@ function preload() {
   anna = loadSound('anna.mp3');
   hoffman = loadSound('hoffman.mp3');
   musica = loadSound('musica.mp3');
+  img_lais = loadImage('lais.png');
+  img_anna = loadImage('anna.png');
+  img_hoffman = loadImage('hoffman.png');
 }
 
 function setup() {
@@ -48,16 +55,26 @@ function setup() {
   amp_hoffman.setInput(hoffman);
   amp_musica = new p5.Amplitude();
   amp_musica.setInput(musica);
+  background(30);
+  imageMode(CENTER);
+  image(img_lais, windowWidth/4, windowHeight/2, 200, 200);
+  image(img_anna, windowWidth/2, windowHeight/2, 200, 200);
+  image(img_hoffman, 3*windowWidth/4, windowHeight/2, 200, 200);
 }
 
 function draw() {
-  background(30);
-  noStroke();
+  noFill();
+  stroke(30);
+  strokeWeight(100);
+  ellipse(windowWidth/4, windowHeight/2, 300, 300);
+  ellipse(windowWidth/2, windowHeight/2, 300, 300);
+  ellipse(3*windowWidth/4, windowHeight/2, 300, 300);
   var vol_lais = amp_lais.getLevel();
   var vol_anna = amp_anna.getLevel();
   var vol_hoffman = amp_hoffman.getLevel();
-  fill(255);
-  ellipse(windowWidth/4, windowHeight/2, 150*vol_lais, 150*vol_lais);
-  ellipse(windowWidth/2, windowHeight/2, 150*vol_anna, 150*vol_anna);
-  ellipse(3*windowWidth/4, windowHeight/2, 150*vol_hoffman, 150*vol_hoffman);
+  strokeWeight(10);
+  stroke(255);
+  ellipse(windowWidth/4, windowHeight/2, 211 + vol_lais*150, 211 + vol_lais*150);
+  ellipse(windowWidth/2, windowHeight/2, 211 + vol_anna*150, 211 + vol_anna*150);
+  ellipse(3*windowWidth/4, windowHeight/2, 211 + vol_hoffman*150, 211 + vol_hoffman*150);
 }
